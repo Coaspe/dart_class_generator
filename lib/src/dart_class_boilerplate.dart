0b6ef4a7367d2caef656f513faa52d22bf971983
Coaspe <aspalt85@gmail.com>
2023-05-16 00:16:22 +0900

    docs: Change comments

--- lib/src/dart_class_boilerplate.dart
+++ lib/src/dart_class_boilerplate.dart
@@ -63,6 +63,7 @@ class DartClassBoilerplate {
     if (constFlag) {
       fields[classLine + 1] = 'const ${fields[classLine + 1]}';
     }
+
     fields.add('});');
 
     if (fields.length <= 3) throw Exception("Has no field");
@@ -84,28 +85,33 @@ class DartClassBoilerplate {
     await file.writeAsString(fields.join('\n'));
   }
 
-  /// Add boilerplate code for json_serializable pacakges
+  /// Generates boilerplate code for json_serializable pacakges
   ///
   /// [folderPath] represents folder that has model classes.
   static void addSerializationStrings(String folderPath) async {
     Uri uri = Uri.parse(folderPath);
     Directory current = Directory.fromUri(uri);
+
     await for (var element in current.list()) {
       File file = File(element.path);
       String fileName = file.path.split("/").last.split('.')[0];
       final readFile = await file.readAsLines();
+
       int classLine = -1;
       int lastCloseBracket = -1;
+
       String partString = "part '$fileName.g.dart';";
       String importString =
           "import 'package:json_annotation/json_annotation.dart';";
       String jsonString = "@JsonSerializable(createToJson: false)";
       String className = "";
+
       for (var i = 0; i < readFile.length; i++) {
         if (classLine == -1 && readFile[i].contains("class ")) {
           classLine = i;
           className = readFile[i].split('class ')[1].split(' ')[0];
         }
+
         if (readFile[i] == '}') {
           lastCloseBracket = i;
         }
@@ -123,15 +129,18 @@ class DartClassBoilerplate {
     }
   }
 
+  /// Generates export code
   static void addModelExport(String folderPath) async {
     Directory current = Directory.fromUri(Uri.parse(folderPath));
     List<String> arr = [];
+
     await for (var element in current.list()) {
       File file = File(element.path);
       String fileName = file.path.split("/").last;
       if (fileName.contains('.g.dart')) continue;
       arr.add("export '$fileName';");
     }
+
     await File('$folderPath/model.dart').writeAsString(arr.join('\n'));
   }
 }
